e5187c0479562bafec47701626fda534e5c5686b
Will Klepinger <33521768+WillKlep@users.noreply.github.com>
2023-02-16 09:48:29 -0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
 
-    //used to initially load the table
-    load_data();
+	$("h1").load(function(event) {
+		event.preventDefault();
+		//used to initially load the table
+	    load_data();
 
-    var run_update = setInterval(load_data, 10000);
+	    var run_update = setInterval(load_data, 10000);
 
-    function load_data() {
-		$.ajax({
-			url: "/data",
-			method: "POST",
-			contentType: "application/json",
-			data:{action: 'fetch'},
-			sucess: function(res){
-				console.log(data);
-				$("h1").html("Quote: ${res.response}");
-			}
-		}) // close ajax
-	} // close load_data
+	    function load_data() {
+			$.ajax({
+				url: "/data",
+				method: "POST",
+				contentType: "application/json",
+				data:{action: 'fetch'},
+				sucess: function(res){
+					console.log(data);
+					$("h1").html("Quote: ${res.response}");
+				}
+			}) // close ajax
+		} // close load_data
+	});// close load
 } // close document.ready
