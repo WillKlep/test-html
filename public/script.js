3a65ef8ba240f0a216679ecff58fc7d49cd968ad
a-mercurio <103204571+a-mercurio@users.noreply.github.com>
2023-02-16 14:13:44 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
 				sucess: function(res){
 					console.log(res.machine_id);
 					
-					document.getElementById("Datamachine_id").innerHTML = res.machine_id;
-					document.getElementById("Datacurrent").innerHTML = res.current;
-					document.getElementById("Datacount").innerHTML = res.count;
+					document.getElementById("Datamachine_id").innerHTML = res.res.machine_id;
+					document.getElementById("Datacurrent").innerHTML = res.res.current;
+					document.getElementById("Datacount").innerHTML = res.res.count;
 					
 					
 					//old method
