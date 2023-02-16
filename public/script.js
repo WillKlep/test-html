cf3057223dd4fbef1b1e2b0a2c7afa234341713a
Will Klepinger <33521768+WillKlep@users.noreply.github.com>
2023-02-16 13:29:23 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
 				sucess: function(res){
 					console.log(data);
 					
-					document.getElementById("Datamachine_id").innerHTML = JSON.stringify(res.data.machine_id);
-					document.getElementById("Datacurrent").innerHTML = JSON.stringify(res.data.current);
-					document.getElementById("Datacount").innerHTML = JSON.stringify(res.data.count);
+					document.getElementById("Datamachine_id").innerHTML = res.data.machine_id;
+					document.getElementById("Datacurrent").innerHTML = res.data.current;
+					document.getElementById("Datacount").innerHTML = res.data.count;
 					
 					
 					//old method
