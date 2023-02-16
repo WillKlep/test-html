eae75dfb0308cf579697bf3438b7f3a3112460a7
a-mercurio <103204571+a-mercurio@users.noreply.github.com>
2023-02-16 13:45:31 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 				contentType: "application/json",
 				data:{action: 'fetch'},
 				sucess: function(res){
-					console.log(data);
+					console.log(res.data.machine_id);
 					
 					document.getElementById("Datamachine_id").innerHTML = res.data.machine_id;
 					document.getElementById("Datacurrent").innerHTML = res.data.current;
